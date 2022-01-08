9d6e7308ae956c7f8d025a098a1a0b7feccf0a2a
wkevin <wkevin27@gmail.com>
2022-01-08 14:47:03 +0800

    [tree-view-sample/jsonOutline.ts] 学习,及绘图

--- tree-view-sample/src/ftpExplorer.ts
+++ tree-view-sample/src/ftpExplorer.ts
@@ -15,15 +15,35 @@ export interface FtpNode {
 
 }
 
+/**
+ * 本例子的关键流程
+ * extension.ts 中实例化 FtpExplorer，自己完成 treeview 的创建，而不是让 vscode 根据 package.json 自动创建。
+ * FtpExplorer 创建 treeview 和 provider，并且实例化 1个 FtpModel 作为 provider 构造函数的入参。
+ * FtpModel 为 provider 提供数据；
+ * provider 继承自2个： TreeviewProvider<FtpNode>、TextDocumentContentProvider
+*/
+
+/**
+ * FtpModel 为 Provider 提供数据，主要实现了3个数据提供函数 roots、getChildren、getContent
+ * 3 个数据提供函数都会在数据获取时 connect() ，获取完毕后自己关闭 ftp 连接（ client.end() )
+ * 其中需要重点理解的是：Promise、Thenable<Client> 
+ */
 export class FtpModel {
 	constructor(readonly host: string, readonly port: number, private user: string, private password: string) {
 	}
 
-	// ???: Thenable<Client> 中的 Client 根据什么定位到 const client 变量的？
-	// 如果是 client（首字母小写）我还能理解，但大写了，谁来判断 client 就是要返回的 Client 呢？
-	// 并且有些情况 Promise 中没有变量的定义，真不知道是根据什么来定义 Promise 中唯一的值的。
-	// 答：
-	// 上面理解有误，c(client) 直接向后面的 p.then(client =>{...}) 传递了 client
+	// 
+	/**
+	 * [Q]
+	 * Thenable<Client> 中的 Client 根据什么定位到 const client 变量的？
+	 * 如果是 client（首字母小写）我还能理解，但大写了，谁来判断 client 就是要返回的 Client 呢？
+	 * 并且有些情况 Promise 中没有变量的定义，真不知道是根据什么来定义 Promise 中唯一的值的。
+	 * [A]
+	 * 上面理解有误，c(client) 直接向后面的 p.then(client =>{...}) 传递了 client
+	 * 并且只表示 c() 传递的 client，不表示 e() 中传递的 string
+	 * Thenable<T> 指 c(<T>) 中返回的 T
+	 * @returns 
+	 */
 	public connect(): Thenable<Client> {
 		return new Promise((c, e) => {
 			const client = new Client();
@@ -36,6 +56,17 @@ export class FtpModel {
 				e('Error while connecting: ' + error.message);
 			});
 
+			/**
+			 * [Q]
+			 * 在这里加断点，单步执行后并不能看到 ftp server 那边的连接显示，而是在 p.then() 的时候才显示，why？
+			 * 比如 下面 root() 中的：
+			 * const p = this.connect();
+			 * p.then(client => {...}, err => {...})
+			 * ftp server 打印：[I 2022-01-10 11:12:40] 127.0.0.1:40346-[123] USER '123' logged in.
+			 * 在 p.then() 之后，c(client) 及 client => {...} 之前。
+			 * [A]
+			 * 画了 ftpExplorer.ts-promise.drawio 感觉理解了，但仔细一想也可能还是不对，具体看图吧。
+			 */
 			client.connect({
 				host: this.host,
 				port: this.port,
@@ -45,9 +76,9 @@ export class FtpModel {
 		});
 	}
 
-	// roots、connect 2个函数和2个Promise 的处理流程参见 promise.drawio
 	public get roots(): Thenable<FtpNode[]> {
 		const p = this.connect();
+		// p.then(func1, func2) 中的2个函数入参，分别对应 Promise 中的 c、e，即：用 c、e 这2张嘴吃掉传递过来的数据
 		return p.then(
 			client => {  // 如何能确认这个 client 就是前面的 const client 呢？—— c(client) 而来，不需要啥确认
 				return new Promise((c, e) => {
@@ -58,7 +89,7 @@ export class FtpModel {
 
 						client.end();
 
-						// 去掉 return 也没啥问题
+						// 去掉 return 这个单词也没啥问题
 						// 新建 Promise 中的 return 有什么特殊的用途么？东西都喂到 c() 的嘴里了，调用者 then 就能吃到了，还 return 给谁呢？
 						// 给创建该 Promise 的进程？所以给不给都行吧——我猜！
 						return c(this.sort(list.map(entry => ({ resource: vscode.Uri.parse(`ftp://${this.host}///${entry.name}`), isDirectory: entry.type === 'd' }))));
@@ -133,7 +164,8 @@ export class FtpModel {
 }
 
 /**
- * 继承自 2 个 interface
+ * 继承自 2 个 interface:
+ * TreeDataProvider: 需实现 getTreeItem、getChildren、getParent
  * TextDocumentContentProvider：为 editor 提供一个只读的文档，需实现 provideTextDocumentContent(文件类型...)
  */
 export class FtpTreeDataProvider implements vscode.TreeDataProvider<FtpNode>, vscode.TextDocumentContentProvider {
@@ -146,7 +178,6 @@ export class FtpTreeDataProvider implements vscode.TreeDataProvider<FtpNode>, vs
 	public refresh(): any {
 		this._onDidChangeTreeData.fire(undefined);
 	}
-
 
 	public getTreeItem(element: FtpNode): vscode.TreeItem {
 		return {
